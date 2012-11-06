4a4791645cb707be1f6b65a6d33de4ff563724a4
Adam DeConinck <ajdecon@ajdecon.org>
2012-11-06 21:55:07 +0000

    Wrote a simple checkmodule script to read module usage

---
+++ checkmodule.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import pymongo
+import sys
+import os
+
+def print_usage():
+    print "USAGE: %s [username]"
+
+# Parse args
+if len(sys.argv)>1:
+    username=sys.argv[1]
+else:
+    username=os.environ['USER'].lower()
+
+# Set up DB connection
+connection = pymongo.Connection('localhost',27017)
+db = connection.modules_log
+users = db.users
+
+u = users.find_one({"username":username})
+if u:
+    print "Module                      Count       Last loaded"
+    print "======                      =====       ==========="
+    for k in u:
+        if k=="username" or k=="_id": 
+            continue
+        print "%-28s  %3d       %s" % (k, u[k]['count'],u[k]['last_loaded'].strftime("%d %h %Y"))
+else:
+    print "No module usage found for %s" % (username)
+    sys.exit(1)
+
+
