31c444c427ca005655b8af36636805039d5b5a40
Adam DeConinck <ajdecon@ajdecon.org>
2012-11-21 14:16:49 -0800

    Added a fix to bail out if modules_logger can't reach the db, so modules operations can continue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="modules-logger",
-      version='0.1',
+      version='0.1.1',
       description="Log usage of environment-modules to MongoDB",
       author="Adam DeConinck",
       author_email="ajdecon@ajdecon.org",
